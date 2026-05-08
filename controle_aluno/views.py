5157a559517d9a7501cba9abeeb1ee143b2c9a2a
brunoasouza68@gmail.com <brunoasouza68@gmail.com>
2026-05-08 08:23:00 -0300

    Add project configuration and dependencies

---
+++ controle_aluno/views.py
@@ -0,0 +1,13 @@
+from typing import List
+from django.shortcuts import render
+from ninja_extra import route, api_controller
+from controle_aluno.models import Aluno
+from controle_aluno.schemas import AlunoOut
+
+# Create your views here.
+
+@api_controller("/controle_aluno",)
+class ControleAlunosView:
+    @route.get("/consultar-alunos", response=List[AlunoOut])
+    def consultar_alunos(self):
+        return Aluno.objects.all()
